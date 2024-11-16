ec0ecf5a798413a82e41dc7ba7de55a683793d3d
JuanMa467 <atenciojuanmanuel05@gmil.com>
2024-11-16 17:50:33 -0300

    7.2

--- funciones.js
+++ funciones.js
@@ -3,19 +3,21 @@ function  tirarDados(){
     // Creo un numero al azar entre el 1 y 6
     numeroAlAzar= Math.floor(Math.random() * (6)) + 1;
     
-    const imgDado =document.getElementById("imgDado")
+    const imgDado = document.getElementById("imgDado")
 
     // Animación de rotación 
-    imgDado.style.transition = 'transform 2s';
+    imgDado.style.transition = 'transform 10s';
     imgDado.style.transform = `rotateZ(${numeroAlAzar * 360}deg) rotateX(${numeroAlAzar * 360}deg)`;
 
     // Cambio la img de dado dependiendo del numero
-    imgDado.src="./dados/dados"+numeroAlAzar+".png";
-
-    // Cambio el resultado
-    const nResultado=document.getElementById("nDado")
-    nResultado.innerHTML=`Resultado: ${numeroAlAzar}`
-    nResultado.style.transition='transform 2s'
+    imgDado.addEventListener('transitionend', () => {
+        // Una vez que la animación termina, cambiamos la imagen
+        imgDado.src = "./dados/dados" + numeroAlAzar + ".png";
+        // Cambio el resultado
+        const nResultado=document.getElementById("nDado")
+        nResultado.innerHTML=`Resultado: ${numeroAlAzar}`
+        nResultado.style.transition='transform 2s'
+    });
 
     // Sonido de tirar dados
     const sonido=document.getElementById("sonidoDado");
